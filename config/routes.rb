3be04e938073c99a5181c915c8b7b60fd0374f07
Kris Foss <kris.foss@gmail.com>
2016-04-19 16:16:51 -0600

    starts splash page and navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   resources :items, only: [:index]
   resources :cart_items, only: [:create]
+
+  root to: 'items#static'
+
   get '/cart', to: 'cart_items#index'
   get '/:category', to: 'categories#show'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
