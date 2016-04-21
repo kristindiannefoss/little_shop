d9e7cee898ab18d4910e58d3f73bcc6af2fe66e6
Kris Foss <kris.foss@gmail.com>
2016-04-20 19:09:31 -0600

    resolves some of the errors for order_item show page, too hungry to finish tonight

--- test/integration/user_can_view_past_orders_items_test.rb
+++ test/integration/user_can_view_past_orders_items_test.rb
@@ -5,6 +5,7 @@ class UserCanViewPastOrdersItemsTest < ActionDispatch::IntegrationTest
     create_user
     user = User.first
     create_orders(1, user.id, "completed")
+    order = Order.first
     # create_orders(1, user.id, "paid")
     # create_orders(1, user.id, "ordered")
     # create_orders(1, user.id, "cancelled")
@@ -18,33 +19,32 @@ class UserCanViewPastOrdersItemsTest < ActionDispatch::IntegrationTest
     #       When I visit "/orders"
     visit '/orders'
     #       Then I should see my past order
-    assert page.has_content?("Order Number: #{Order.first.id}")
+    assert page.has_content?("Order Number: #{order.id}")
     #       And I should see a link to view that order
     #view order link
     assert page.has_link?("View Order Details")
     #       And when I click that link
     click_link "View Order Details"
     #       Then I should see each item that was ordered
-    assert page.has_content("Order Details:")
-    assert page.has_content("#{order.items.first.name}")
+    assert page.has_content?("#{order.items.first.name}")
     #       And I should see links to each item's show page
     assert page.has_link?("#{order.items.first.name}")
     #with the quantity and line-item subtotals
-    assert page.has_content("Quantity: #{order.items.first.quantity}")
-    assert page.has_content("Subtotal: #{order.subtotals}")
+    assert page.has_content?("Quantity: #{order.items.first.quantity}")
+    assert page.has_content?("Subtotal: #{order.subtotals}")
     #       And I should see the current status of the order (ordered, paid, cancelled, completed)
-    assert page.has_content("ordered")
-    assert page.has_content("paid")
-    assert page.has_content("cancelled")
-    assert page.has_content("completed")
+    assert page.has_content?("ordered")
+    assert page.has_content?("paid")
+    assert page.has_content?("cancelled")
+    assert page.has_content?("completed")
     #       And I should see the total price for the order
-    assert page.has_content("Order Total: #{order.total_price}")
+    assert page.has_content?("Order Total: #{order.total_price}")
     #       And I should see the date/time that the order was submitted
-    assert page.has_content("Order Date: #{order.created_at}")
+    assert page.has_content?("Order Date: #{order.created_at}")
     #       If the order was completed or cancelled
-    assert page.has_content("Order Status#{order.order_status}")
+    assert page.has_content?("Order Status#{order.order_status}")
     #       Then I should see a timestamp when the action took place
-    assert page.has_content("#{order.updated_at}")
+    assert page.has_content?("#{order.updated_at}")
     #
   end
 end
