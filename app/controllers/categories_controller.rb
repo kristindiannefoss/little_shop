39cb5a03efa6ea85a54e85455403e87a7d8e77e5
Mark Miranda <notmarkmiranda@gmail.com>
2016-04-22 21:34:30 -0600

    add testing to ensure user-to-user security

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,5 +2,6 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find_by(name: params[:category])
+    redirect_to '/errors/not_found.html' unless @category
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,7 +16,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    session.clear
+    session.delete :user_id
     redirect_to items_path
   end
 
