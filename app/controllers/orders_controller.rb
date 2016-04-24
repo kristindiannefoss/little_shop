1b4fa5be105dc6f847adb9d3fb005c2eae6d5133
Mark Miranda <notmarkmiranda@gmail.com>
2016-04-24 15:34:01 -0600

    49 checkout (#66)
    * start checkout functionality
    
    * user checkout test

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,7 +7,32 @@ class OrdersController < ApplicationController
   end
 
   def show
-    @order = Order.find(params[:id])
+    @order = current_user.orders.find_by(id: params[:format].to_i)
+
+    # byebug
+
+    # @order = Order.find_by(order_id: params[:id], )
     @order_items = OrderItem.where(order_id: @order.id)
+  end
+
+  def create
+    @order = current_user.orders.new
+    if @order.save
+      @cart.contents.each_pair do |item_id, item_qnt|
+        @order.order_items.create(item_id: item_id.to_i, quantity: item_qnt)
+        @order.update(status: "ordered")
+      end
+      flash[:notice] = "Order was successfully placed"
+      redirect_to orders_path
+    else
+      flash.now[:error] = "Please log in to place an order"
+      redirect_to login_path
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:user_id)
   end
 end
