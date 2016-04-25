0e31b3fa981dd4f17edb09f2ed71741c0ff9c6ca
Kris Foss <kris.foss@gmail.com>
2016-04-25 15:53:10 -0600

    fix hound complaints

--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -1,25 +1,21 @@
-require 'test_helper'
+require "test_helper"
 
 class ItemTest < ActiveSupport::TestCase
-  # create_categories
 
   test "Correct item attributes" do
     item = Item.create(
     name: "Bronto Burgers",
     description: "Yum, meaty",
     image_url: "sample",
-    price: 6.00,
-    # category_id: "1"
-    )
-
-
-  assert item.valid?
-  assert "How do you test for the attributes in a model test??"
-  #   - An item must have a title, description and price.
-  # - An item must belong to at least one category.
-  # - The title and description cannot be empty.
-  # - The title must be unique for all items in the system.
-  # - The price must be a valid decimal numeric value and greater than zero.
-  # - The photo is optional. If not present, a stand-in photo is used. (PAPERCLIP)
+    price: 6.00)
+    assert item.valid?
+    assert "How do you test for the attributes in a model test??"
+    #   - An item must have a title, description and price.
+    # - An item must belong to at least one category.
+    # - The title and description cannot be empty.
+    # - The title must be unique for all items in the system.
+    # - The price must be a valid decimal numeric value and greater than zero.
+    # - The photo is optional.
+    # - If not present, a stand-in photo is used. (PAPERCLIP)
   end
 end
