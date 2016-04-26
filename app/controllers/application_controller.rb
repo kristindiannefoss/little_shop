aece5502d401fd60f1a8097e373f833181939732
Kris Foss <kris.foss@gmail.com>
2016-04-26 16:24:09 -0600

    styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
   before_action :set_cart
 
   helper_method :current_user, :set_redirect, :current_admin?
